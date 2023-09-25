32572d52f6d1d71a2556be2a454e1d4bf8c6a145
Ravindra <ravindrapawar1315@gmail.com>
2023-09-25 09:59:05 +0530

    psycopg2 package added and installed

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+def add(x,y):
+    """
+    Adding two numbers and returns the result.
+    """
+    return x + y
+
+def substract(x,y):
+    """
+    Substracting the two numbers together and returns the result.
+    """
+    return abs(x-y)
