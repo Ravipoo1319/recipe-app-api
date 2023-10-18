4f7aae31aeef618c937966baaef549bd69ce204f
Ravindra <ravindrapawar1315@gmail.com>
2023-10-18 10:23:43 +0530

    Handled user management functionality.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -18,6 +18,18 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return user."""
+
+        # Extract the password from the validated data dictionary if it exists.
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
